3b4ce0a8b012fcb39a6bd1bf4e41d585f395891e
JY-M6 <m15723155100_1@163.com>
2025-12-06 16:40:02 +0800

    提交实验6第一项作业

--- school/lab6.1/src/Main.java
+++ school/lab6.1/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your score");
+        int n = 0;
+        try {
+            n = sc.nextInt();
+            if (!(n <= 100 && n >= 0)){
+                throw new MyException();
+            }
+            System.out.println("Your score is:" + n );
+        } catch (Exception e) {
+            System.out.println("Error,score should be in 0-100.");
+
+    }
+    }
+}
+
+class MyException extends RuntimeException {
+    public MyException() {
+        super();
+    }
+
+    public MyException(String message) {
+        super(message);
     }
 }
