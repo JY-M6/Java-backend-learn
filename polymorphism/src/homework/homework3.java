63d19498905dccd2c9a0a94c57f59e650db05c4a
JY-M6 <m15723155100_1@163.com>
2025-12-08 16:58:31 +0800

    Delete polymorphism directory

--- polymorphism/src/homework/homework3.java
+++
@@ -1,8 +0,0 @@
-package homework;
-
-public class homework3 {
-    public static void main(String[] args) {
-    Professor professor = new Professor("a",30,"职称",30000,1.3);
-    professor.introduce();
-    }
-}
