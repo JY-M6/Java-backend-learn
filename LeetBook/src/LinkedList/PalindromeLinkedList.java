109dff9dc1585d3fce30a7d0beb68d6dd6dd1521
JY-M6 <m15723155100_1@163.com>
2025-12-11 12:58:32 +0800

    力扣234

--- LeetBook/src/LinkedList/PalindromeLinkedList.java
+++ LeetBook/src/LinkedList/PalindromeLinkedList.java
@@ -34,3 +34,63 @@ class ListNode_234 {
       ListNode_234(int val, ListNode_234 next) {
           this.val = val; this.next = next; }
   }
+
+//最快解
+class Solution {
+    public boolean isPalindrome(ListNode_234 head) {
+        if(null==head || null==head.next)
+            return true;
+
+        ListNode_234 slow=head,quick=head;
+
+        while(quick!=null && quick.next!=null){
+            quick=quick.next.next;
+            slow=slow.next;
+        }
+        //LinkListUtil.printLinkedList(slow);
+        //	if(quick==null){
+        // 偶数个
+        //slow=slow.next;
+        slow=reverseLinkedList(slow);
+        //	}else{
+        // 奇数个
+        //		slow=reverseLinkedList(slow);
+        //	}
+
+        //System.out.println("slow");LinkListUtil.printLinkedList(slow);
+        //	System.out.println("head");LinkListUtil.printLinkedList(head);
+
+        while(slow!=null){
+            if(slow.val!=head.val)
+                return false;
+            slow=slow.next;
+            head=head.next;
+        }
+
+
+        return true;
+    }
+
+    /**
+     * 反转链表
+     *
+     * @param head
+     * @return
+     */
+
+    public ListNode_234 reverseLinkedList(ListNode_234 head){
+        if(head==null||head.next==null)
+            return head;
+
+        ListNode_234 last=null;
+        while(head!=null){
+            ListNode_234 tmp=head.next;
+            head.next=last;
+            last=head;
+            head=tmp;
+
+        }
+
+        return last;
+    }
+}
