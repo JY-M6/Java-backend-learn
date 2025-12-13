08f0ce138bdf07a57c80dafba269102b331b7edd
JY-M6 <m15723155100_1@163.com>
2025-12-13 16:59:07 +0800

    设计模式————固定顺序输出（ReentrantLock）

--- Thread_plus/src/ChapterFour/AlternatingOutput/ReentrantLockWay.java
+++ Thread_plus/src/ChapterFour/AlternatingOutput/ReentrantLockWay.java
@@ -5,23 +5,23 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockWay {
     public static void main(String[] args) throws InterruptedException {
+        System.out.println("开始...");
         AwaitSignal awaitSignal = new AwaitSignal(5);
         Condition a = awaitSignal.newCondition();
         Condition b = awaitSignal.newCondition();
         Condition c = awaitSignal.newCondition();
         new Thread(() -> {
-            awaitSignal.print("a",a,b);
+            awaitSignal.print("a",0,a,b);
         }).start();
         new Thread(() -> {
-            awaitSignal.print("b",b,c);
+            awaitSignal.print("b",1,b,c);
         }).start();
         new Thread(() -> {
-            awaitSignal.print("c",c,a);
+            awaitSignal.print("c",2,c,a);
         }).start();
         Thread.sleep(1000);
         awaitSignal.lock();
         try {
-            System.out.println("开始...");
             a.signal();
         } finally {
             awaitSignal.unlock();
@@ -30,15 +30,19 @@ public class ReentrantLockWay {
 }
 class AwaitSignal extends ReentrantLock {
     private int loopNumber;
+    private int flag = 0;
     public AwaitSignal(int loopNumber) {
         this.loopNumber = loopNumber;
     }
-    public void print(String str, Condition current, Condition next) {
+    public void print(String str, int target, Condition current, Condition next) {
         for (int i = 0; i < loopNumber; i++) {
             lock();
             try {
-                current.await();
+                while (flag != target) {
+                    current.await();
+                }
                 System.out.print(str);
+                flag = (flag + 1) % 3;
                 next.signal();
             } catch (InterruptedException e) {
                 e.printStackTrace();
