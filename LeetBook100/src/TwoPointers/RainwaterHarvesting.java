f401be2dd602b0a0b9aed07f8f5e600a2fa682eb
JY-M6 <m15723155100_1@163.com>
2026-01-11 15:09:27 +0800

    力扣42

---
+++ LeetBook100/src/TwoPointers/RainwaterHarvesting.java
@@ -0,0 +1,33 @@
+package TwoPointers;
+//力扣42
+public class RainwaterHarvesting {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int total = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    total += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    total += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return total;
+    }
+}
