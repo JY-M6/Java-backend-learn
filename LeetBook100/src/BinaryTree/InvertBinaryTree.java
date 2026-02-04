8a0e4880830a58b3ac925c5814dfc12361c93a14
JY-M6 <m15723155100_1@163.com>
2026-02-04 11:18:13 +0800

    力扣226

---
+++ LeetBook100/src/BinaryTree/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+package BinaryTree;
+//力扣226
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp ;
+        temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
