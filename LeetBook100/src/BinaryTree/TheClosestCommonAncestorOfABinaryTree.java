ac5370d116cd9870e50bb46f2fabc49378a94442
JY-M6 <m15723155100_1@163.com>
2026-03-11 11:14:06 +0800

    力扣236

---
+++ LeetBook100/src/BinaryTree/TheClosestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,18 @@
+package BinaryTree;
+//力扣236
+
+public class TheClosestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q){
+            return root;
+    }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null){
+            return root;
+        }
+
+            return left != null ? left : right;
+    }
+}
